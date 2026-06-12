4971cba1e81782dc963fc1c7880b84b13bd5402a
jhonatan-cyber <jhonatanancasi@gmail.com>
2026-06-12 14:41:42 -0400

    fix(security): add Cloudflare Insights to CSP script-src

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -29,7 +29,7 @@ class SecurityHeaders
 
         $response->headers->set('Content-Security-Policy',
             "default-src 'self'; " .
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://us-assets.i.posthog.com https://eu-assets.i.posthog.com{$devCsp}; " .
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net https://static.cloudflareinsights.com https://us-assets.i.posthog.com https://eu-assets.i.posthog.com{$devCsp}; " .
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " .
             "img-src 'self' data: blob: https: https://us-assets.i.posthog.com https://eu-assets.i.posthog.com; " .
             "font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net; " .
